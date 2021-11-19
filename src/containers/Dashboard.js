7e1cd0d4f63a01ff4489da94b95f8f1b23d2f907
boris_montavon <boris.montavon@gmail.com>
2021-11-19 11:39:55 +0100

    [+] Img fix in css and removed useless functions in render

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -91,9 +91,8 @@ export default class {
 
   handleClickIconEye = () => {
     const billUrl = $('#icon-eye-d').attr("data-bill-url");
-    const imgWidth = Math.floor($('#modaleFileAdmin1').width() * 0.8);
 
-    $('#modaleFileAdmin1').find(".modal-body").html(`<div style='text-align: center;'><img width=${imgWidth} src=${billUrl} /></div>`);
+    $('#modaleFileAdmin1').find(".modal-body").html(`<div style='text-align: center;'><img src=${billUrl} /></div>`);
 
     if (typeof $('#modaleFileAdmin1').modal === 'function') {
       $('#modaleFileAdmin1').modal('show');
